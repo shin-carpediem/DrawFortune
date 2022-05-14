981d8bbda2f38b556e39b49d64e308348636e16a
shin-carpediem <buru.aoshin@gmail.com>
2022-05-14 17:34:22 +0900

    first commit

---
+++ DrawFortune/DrawFortuneUITests/DrawFortuneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DrawFortuneUITestsLaunchTests.swift
+//  DrawFortuneUITests
+//
+//  Created by 青木振一郎 on 2022/05/14.
+//
+
+import XCTest
+
+class DrawFortuneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
