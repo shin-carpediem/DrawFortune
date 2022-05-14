29dd83925ff7a00c624e930508025e7cbeb18306
shin-carpediem <buru.aoshin@gmail.com>
2022-05-14 17:37:48 +0900

    remove comment

--- DrawFortune/DrawFortune/ViewController.swift
+++ DrawFortune/DrawFortune/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  DrawFortune
-//
-//  Created by 青木振一郎 on 2022/05/14.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
--- DrawFortune/DrawFortuneUITests/DrawFortuneUITestsLaunchTests.swift
+++ DrawFortune/DrawFortuneUITests/DrawFortuneUITestsLaunchTests.swift
@@ -1,10 +1,3 @@
-//
-//  DrawFortuneUITestsLaunchTests.swift
-//  DrawFortuneUITests
-//
-//  Created by 青木振一郎 on 2022/05/14.
-//
-
 import XCTest
 
 class DrawFortuneUITestsLaunchTests: XCTestCase {
@@ -20,9 +13,6 @@ class DrawFortuneUITestsLaunchTests: XCTestCase {
     func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
 
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
