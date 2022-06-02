8e2b363f1c2adf6b22161527646ab863c7de3fdf
shin-carpediem <buru.aoshin@gmail.com>
2022-06-03 00:43:11 +0900

    タイマーのラベルを準備

--- DrawFortune/DrawFortune/Modules/Top/View/TimerViewController.swift
+++ DrawFortune/DrawFortune/Modules/Top/View/TimerViewController.swift
@@ -14,14 +14,42 @@ final class TimerViewController: UIViewController {
     
     // MARK: private
     
-    private lazy var timerNumber: UILabel = {
+    private let timerNumber: UILabel = {
         let view = UILabel()
-        view.text = "Time"
+        let string = NSMutableAttributedString()
+        string.append(NSMutableAttributedString(
+            string: "00:00:00",
+            attributes: [.font: UIFont.systemFont(ofSize: 24)]
+        ))
+        view.attributedText = string
         return view
     }()
     
-    private lazy var HstackView: UIStackView = {
+    private let HstackView: UIStackView = {
         let view = UIStackView()
+        view.axis = .horizontal
+        view.distribution = .fillEqually
+        return view
+    }()
+    
+    private let startButton: UIButton = {
+        let view = UIButton()
+        view.setTitleColor(.blue, for: .normal)
+        view.setTitle("Start", for: .normal)
+        return view
+    }()
+    
+    private let stopButton: UIButton = {
+        let view = UIButton()
+        view.setTitleColor(.blue, for: .normal)
+        view.setTitle("Stop", for: .normal)
+        return view
+    }()
+    
+    private let resetButton: UIButton = {
+        let view = UIButton()
+        view.setTitleColor(.blue, for: .normal)
+        view.setTitle("Reset", for: .normal)
         return view
     }()
         
@@ -33,13 +61,20 @@ final class TimerViewController: UIViewController {
 
         view.addSubview(timerNumber)
         view.addSubview(HstackView)
+        HstackView.addArrangedSubview(startButton)
+        HstackView.addArrangedSubview(stopButton)
+        HstackView.addArrangedSubview(resetButton)
 
         NSLayoutConstraint.activate([
             timerNumber.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             timerNumber.bottomAnchor.constraint(equalTo: HstackView.topAnchor, constant: -10),
 
             HstackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            HstackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            HstackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            HstackView.topAnchor.constraint(equalTo: timerNumber.bottomAnchor),
+            HstackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            HstackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            HstackView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
 }
