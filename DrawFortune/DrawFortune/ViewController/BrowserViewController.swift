4bacc2c9d87487852011cd76f3460c4a87a279f9
shin-carpediem <buru.aoshin@gmail.com>
2022-06-04 18:48:47 +0900

    理解できている所だけ残す

--- DrawFortune/DrawFortune/ViewController/BrowserViewController.swift
+++ DrawFortune/DrawFortune/ViewController/BrowserViewController.swift
@@ -2,33 +2,21 @@ import UIKit
 import WebKit
 
 //https://developer.apple.com/documentation/webkit/wkwebview
-final class BrowserViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
+final class BrowserViewController: UIViewController, WKUIDelegate {
     var webView: WKWebView!
     var urlString: String = "https://google.com"
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        if webView.canGoBack {
-            backBtn.tintColor = nil
-        } else {
-            backBtn.tintColor = .gray
-        }
-    }
-    
+        
     // MARK: override
     
     override func loadView() {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.uiDelegate = self
-        webView.navigationDelegate = self
-        webView.allowsBackForwardNavigationGestures = true
         view = webView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupLayout()
-        createToolBarItems()
         openUrl(urlString)
     }
     
@@ -42,44 +30,10 @@ final class BrowserViewController: UIViewController, WKNavigationDelegate, WKUID
         return view
     }()
     
-    private let backBtn = UIBarButtonItem(image: UIImage(named: "back"),
-                                          style: .plain,
-                                          target: BrowserViewController.self,
-                                          action: #selector(goBack))
-    
-    private func setupLayout() {
-        view.backgroundColor = .white
-    }
-    
-    private func createToolBarItems() {
-        toolbarItems = [
-            backBtn,
-            UIBarButtonItem(image: UIImage(named: "next"),
-                            style: .plain,
-                            target: self,
-                            action: #selector(goForward)),
-            UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
-                            target: nil,
-                            action: nil),
-            UIBarButtonItem(barButtonSystemItem: .refresh,
-                            target: self,
-                            action: #selector(refresh))
-        ]
-    }
-    
     private func openUrl(_ string: String?) {
         guard let string = string else { return }
         let url = URL(string: string)
         let request = URLRequest(url: url!)
         webView.load(request)
-    }
-    
-    @objc private func goBack() {
-    }
-    
-    @objc private func goForward() {
-    }
-    
-    @objc private func refresh() {
     }
 }
